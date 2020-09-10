85085083ef14ab3740e6bce7253d713f43cc22c1
milkpanth <milkpanth@yahoo.com>
2020-09-10 21:58:50 +0700

    logo

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -9,9 +9,14 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Sign In'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[],
+          ),
+        ));
   }
 }

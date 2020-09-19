676cb1280d06c6a356f3c6796247de8862a41ff6
milkpanth <milkpanth@yahoo.com>
2020-09-20 01:20:53 +0700

    gasmenu_shop

---
+++ lib/widget/listgas_shop.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListGasType extends StatefulWidget {
+  @override
+  _ListGasTypeState createState() => _ListGasTypeState();
+}
+
+class _ListGasTypeState extends State<ListGasType> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Gas Menu');
+  }
+}

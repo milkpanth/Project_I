087c77d29721b72ddb40f97731f06f49ab7b797a
milkpanth <milkpanth@yahoo.com>
2020-09-19 15:48:08 +0700

    home_edit2

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gastogo/screens/signin.dart';
 import 'package:gastogo/screens/signup.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +9,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkPreference();
+  }
+
+  Future<Null> checkPreference() async {
+    try {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      String showName = preferences
+          .getString('Name'); //showว่าเปนผู้คนใช้คนไหนหรือเจ้าของร้าน
+      if (showName != null && showName.isEmpty) {}
+    } catch (e) {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
